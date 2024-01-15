9476dcbcad9e91aae9f91ab59997dd1861414df4
Radu Albu <radu.albu@money-phone.com>
2024-01-16 01:28:26 +0200

    validator patched

--- enBW/events/models.py
+++ enBW/events/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     uuid = models.CharField(editable=False, max_length=30)
-    title = models.CharField(max_length = 100, default=None, blank=True)
+    title = models.CharField(max_length = 100, default=None, blank=False)
     message = models.TextField()
