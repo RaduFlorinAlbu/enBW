9801a2477ae73ddea796a4c2dcae0114356b42d8
Radu Albu <radu.albu@money-phone.com>
2024-01-15 20:54:52 +0200

    First model and admin page

--- enBW/events/models.py
+++ enBW/events/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    uuid = models.CharField(editable=False, max_length=30)
+    title = models.CharField(max_length = 100, default=None, blank=True)
+    message = models.TextField()
