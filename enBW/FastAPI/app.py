4a4622d80a3f10d6408eb978c76c22fca512b3ad
Radu Albu <radu.albu@money-phone.com>
2024-01-16 20:34:16 +0200

    Angular Added

--- enBW/FastAPI/app.py
+++ enBW/FastAPI/app.py
@@ -3,6 +3,7 @@ setup_django() # moved on top because models imports depend on it
 from fastapi import FastAPI, HTTPException
 from events.models import Event
 from buckets.models import Bucket
+from django.forms.models import model_to_dict
 
 
 
@@ -15,3 +16,20 @@ def read_event(event_id: int):
         return {"event_name": event.title, "event_message": event.message}
     except Event.DoesNotExist:
         raise HTTPException(status_code=404, detail="Event not found")
+    
+@app.get("/buckets/{bucket_name}")
+def read_event(bucket_name: str):
+    try:
+        bucket = Bucket.objects.get(name=bucket_name)
+    except Bucket.DoesNotExist:
+        raise HTTPException(status_code=404, detail="Bucket not found") 
+    events_info = [{"title": event_dict["title"], "message": event_dict["message"]} for event in bucket.events.all() for event_dict in [model_to_dict(event)]]
+    return events_info
+    
+@app.put("/{bucket_name}")
+def store_event(bucket_name: str, event_title: str, event_message: str):
+    try:
+        bucket = Bucket.objects.get(name=bucket_name)
+    except Bucket.DoesNotExist:
+        raise HTTPException(status_code=404, detail="Bucket not found")
+          
