e0b3d6ad7201d668c433110e9a5905f3001326c5
Radu Albu <radu.albu@money-phone.com>
2024-01-16 13:56:11 +0200

    FastAPI Integrated, StaticFiles created and placed

--- enBW/enBW/asgi.py
+++ enBW/enBW/asgi.py
@@ -10,7 +10,22 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enBW.settings')
 
-application = get_asgi_application()
+django_app = get_asgi_application()
+fastapi_app = FastAPI() 
+
+print("Mounting before happening")
+fastapi_app.mount("/static", StaticFiles(directory="static"), name="static")
+fastapi_app.mount("/django", django_app)
+
+
+
+@fastapi_app.get("/api")
+async def read_api():
+    return { "message": "First FastAPI Call"}
+
+application = fastapi_app
